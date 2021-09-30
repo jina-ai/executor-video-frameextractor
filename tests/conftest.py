8a2b1ca95fbb786b39032b5134532d1f7da2e1a7
makram93 <kalimakram@gmail.com>
2021-09-30 10:37:48 +0200

    fix(test): apply suggestions from code review

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,4 @@
 import pytest
-from jina import Document, DocumentArray
 
 from executor import VideoFrameExtractor
 
@@ -9,13 +8,3 @@ def encoder(tmp_path) -> VideoFrameExtractor:
     workspace = str(tmp_path / 'workspace')
     encoder = VideoFrameExtractor(metas={'workspace': workspace})
     return encoder
-
-
-@pytest.fixture(scope='package')
-def build_da():
-    def _build_da():
-        return DocumentArray(
-            [Document(id='2c2OmN49cj8.mp4', uri='tests/toy_data/2c2OmN49cj8.mp4')]
-        )
-
-    return _build_da
--- tests/integration/test_flow.py
+++ tests/integration/test_flow.py
@@ -1,15 +1,19 @@
 __copyright__ = 'Copyright (c) 2021 Jina AI Limited. All rights reserved.'
 __license__ = 'Apache-2.0'
 
-from jina import Flow
+from jina import Document, DocumentArray, Flow
 
 from executor import VideoFrameExtractor
 
 
-def test_integration(tmp_path, build_da):
-    da = build_da()
+def test_integration(tmp_path):
+    da = DocumentArray(
+        [Document(id='2c2OmN49cj8.mp4', uri='tests/toy_data/2c2OmN49cj8.mp4')]
+    )
     workspace = str(tmp_path / 'workspace')
-    with Flow().add(uses=VideoFrameExtractor, metas={'workspace': workspace}) as flow:
+    with Flow().add(
+        uses=VideoFrameExtractor, uses_metas={'workspace': workspace}
+    ) as flow:
         resp = flow.post(on='/index', inputs=da, return_results=True)
 
     assert len(resp[0].docs) == 1
